87dc9eaa2006a65de6ac5eceae0231a9e58c5f00
原田悠嗣 <bikkirisaburou@gmail.com>
2019-02-21 00:39:37 +0900

    Add Direction Action

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var basicCard: UIView!
 
+    @IBOutlet weak var likeImageView: UIImageView!
+
+
+
     var centerOfCard:CGPoint!
 
 
@@ -34,6 +38,21 @@ class ViewController: UIViewController {
 
         card.center = CGPoint(x: card.center.x + point.x, y:card.center.y + point.y)
 
+        // 元々の位置とい移動先との差（角度を変える）
+        let xfromCenter = card.center.x - view.center.x
+        // 0.785は45度のラジアン
+        card.transform = CGAffineTransform(rotationAngle: xfromCenter / (view.frame.width / 2) * -0.785)
+
+        if xfromCenter > 0 {
+            likeImageView.image = #imageLiteral(resourceName: "good")
+            likeImageView.alpha = 1
+            likeImageView.tintColor = UIColor.red
+        } else if xfromCenter < 0 {
+            likeImageView.image = #imageLiteral(resourceName: "bad")
+            likeImageView.alpha = 1
+            likeImageView.tintColor = UIColor.blue
+        }
+
         if sender.state == UIGestureRecognizer.State.ended {
             // 左に大きくスワイプ
             if card.center.x < 75 {
@@ -52,8 +71,9 @@ class ViewController: UIViewController {
             // 元に戻る処理
             UIView.animate(withDuration: 0.2, animations: {
                 card.center = self.centerOfCard
+                card.transform = .identity
             })
-            card.center = centerOfCard
+            likeImageView.alpha = 0
         }
 
     }
