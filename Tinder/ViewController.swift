f4b6225ac8f30a6e9c4c4e6bed4519e6a24b64ef
原田悠嗣 <bikkirisaburou@gmail.com>
2019-02-23 12:59:01 +0900

    Add Swipe Logic to All Cards

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
     @IBOutlet weak var basicCard: UIView!
-
     @IBOutlet weak var likeImageView: UIImageView!
 
-
+    @IBOutlet weak var person1: UIView!
+    @IBOutlet weak var person2: UIView!
+    @IBOutlet weak var person3: UIView!
+    @IBOutlet weak var person4: UIView!
 
     var centerOfCard:CGPoint!
+    var people = [UIView]()
+    var selectedCardCount:Int = 0
+
 
 
 
@@ -25,6 +29,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         centerOfCard = basicCard.center
+        people.append(person1)
+        people.append(person2)
+        people.append(person3)
+        people.append(person4)
+    }
+
+
+    func resetCard() {
+    basicCard.center = self.centerOfCard
+    basicCard.transform = .identity
     }
 
   
@@ -37,11 +51,16 @@ class ViewController: UIViewController {
         let point = sender.translation(in: view)
 
         card.center = CGPoint(x: card.center.x + point.x, y:card.center.y + point.y)
+        people[selectedCardCount].center = CGPoint(x: card.center.x + point.x, y:card.center.y + point.y)
+
+
 
         // 元々の位置とい移動先との差（角度を変える）
         let xfromCenter = card.center.x - view.center.x
+
         // 0.785は45度のラジアン
         card.transform = CGAffineTransform(rotationAngle: xfromCenter / (view.frame.width / 2) * -0.785)
+        people[selectedCardCount].transform = CGAffineTransform(rotationAngle: xfromCenter / (view.frame.width / 2) * -0.785)
 
         if xfromCenter > 0 {
             likeImageView.image = #imageLiteral(resourceName: "good")
@@ -57,21 +76,29 @@ class ViewController: UIViewController {
             // 左に大きくスワイプ
             if card.center.x < 75 {
                 UIView.animate(withDuration: 0.2, animations: {
-                    card.center = CGPoint(x: card.center.x - 250,y: card.center.y)
+                    self.resetCard()
+                    self.people[self.selectedCardCount].center = CGPoint(x:self.people[self.selectedCardCount].center.x - 400, y:self.people[self.selectedCardCount].center.y)
                 })
+                likeImageView.alpha = 0
+                selectedCardCount += 1
                 return
             // 右に大きくスワイプ
             } else if card.center.x > self.view.frame.width - 75 {
                 UIView.animate(withDuration: 0.2, animations: {
-                    card.center = CGPoint(x: card.center.x - 250,y: card.center.y)
+                    self.resetCard()
+                    self.people[self.selectedCardCount].center = CGPoint(x:self.people[self.selectedCardCount].center.x + 400, y:self.people[self.selectedCardCount].center.y)
+                    //card.center = CGPoint(x: card.center.x - 250,y: card.center.y)
                 })
+                likeImageView.alpha = 0
+                selectedCardCount += 1
                 return
             }
 
             // 元に戻る処理
             UIView.animate(withDuration: 0.2, animations: {
-                card.center = self.centerOfCard
-                card.transform = .identity
+                self.resetCard()
+                self.people[self.selectedCardCount].center = self.centerOfCard
+                self.people[self.selectedCardCount].transform = .identity
             })
             likeImageView.alpha = 0
         }
